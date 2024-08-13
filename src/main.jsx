ef95eb4892bbcbf7fadec73db5528fd808eb2ff1
antoniolsteixeira <antonioluisteixeira@hotmail.com>
2024-08-12 21:26:00 -0300

    change of stylization

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Details } from './pages/Details'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
 import { ThemeProvider } from 'styled-components';
+
+import GlobalStyles from './styles/global'
+
+import { Details } from './pages/Details';
 
 import theme from './styles/theme';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <Details />
     </ThemeProvider>
   </StrictMode>,
