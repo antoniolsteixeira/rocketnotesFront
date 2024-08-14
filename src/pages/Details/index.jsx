6e7e686600d738165a1b516ad4dfe730fa4ba895
Antônio Luis Souza Teixeira <antonio.teixeira@foa.org.br>
2024-08-13 22:04:06 -0300

    update13082024

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,9 +1,12 @@
-import { Container } from "./styles";
+import { Container } from "./styled";
+
+import { Header } from '../../components/Header'
 import { Button } from "../../components/Button";
 
 export function Details() {
   return (
     <Container>
+      <Header />
       <h1>Hello World!</h1>
       <span>Antonio Teixeira</span>
       <Button title="Voltar" />
